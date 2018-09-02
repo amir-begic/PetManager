d6386f6ac01b48099fcc2ebb4693fd0a0a7e8901
Amir Begic <amir.begic@namics.com>
2018-09-02 16:56:07 +0200

    Added Equals

--- PetManager/Pet.cs
+++ PetManager/Pet.cs
@@ -8,5 +8,28 @@ namespace PetManager {
     public class Pet {
         public string Name { get; set; }
         public string Breed { get; set; }
+
+        public override int GetHashCode()
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 17;
+                hash = hash * 23 + (this.Lastname != null ? this.Lastname.GetHashCode() : 0);
+                hash = hash * 23 + (this.Firstname != null ? this.Firstname.GetHashCode() : 0);
+                hash = hash * 23 + (this.Pets != null ? this.Pets.GetHashCode() : 0);
+                return hash;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Pet))
+            {
+                return object.Equals(obj, this);
+            }
+            var pet = (Pet)obj;
+            return Birthday.Equals(pet.Birthday) && string.Equals(this.Name, pet.Name) &&
+            string.Equals(this.Breed, pet.Breed);
+        }
     }
 }
